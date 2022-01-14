5d5b50754fcdb63f707c67dd8ecfb4e309eec2d2
danfoy <contact@danfoy.com>
2022-01-14 16:23:37 +0000

    refactor: follow discordjs naming conventions
    I was calling my client 'discord', but as the dolores module is now
    standalone this becomes more confusing than useful, so I'm reverting to
    the conventions suggested by the discordjs package.

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const { Client, Collection, Intents } = require('discord.js');
 const { bot } = require('./config.json');
 
 // Create the discordjs client instance
-const discord = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Attach commands to client
-discord.commands = new Collection();
+client.commands = new Collection();
 glob.sync('./commands/**/*.command.js')
 	.forEach(file => {
 		const command = require(file);
-		return discord.commands.set(command.meta.name, command);
+		return client.commands.set(command.meta.name, command);
 	});
-const commandsCount = discord.commands.size;
+const commandsCount = client.commands.size;
 console.log(`Found ${commandsCount} ${ commandsCount > 1 && commandsCount != 0 ? 'commands' : 'command'} definition`);
 
 // Register event handlers
 glob.sync('./events/**/*.event.js')
 	.forEach(file => {
 		const event = require(file);
-		if (event.once) return discord.once(event.name, (...args) => event.execute(...args));
-		return discord.on(event.name, (...args) => event.execute(...args));
+		if (event.once) return client.once(event.name, (...args) => event.execute(...args));
+		return client.on(event.name, (...args) => event.execute(...args));
 	});
 
 // Attempt login
-discord.login(bot.token);
+client.login(bot.token);
