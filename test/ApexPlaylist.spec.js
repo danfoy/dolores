ea7893102e4ce3dea93b8edb83ee0cdfaf42b6f9
danfoy <contact@danfoy.com>
2022-01-15 15:10:51 +0000

    test(maps): add tests for map finder
    I tracked a day's worth of map rotations manually on 2021-01-11. By
    adding these as tests I will be able to work out the start time of
    Season 11, and then add that to my dataset.

---
+++ test/ApexPlaylist.spec.js
@@ -0,0 +1,55 @@
+const { expect } = require('chai');
+const ApexPlaylist = require('../commands/map/ApexPlaylist');
+
+describe('@ApexPlaylist', function() {
+
+    it("provide correct values for Season 11 'Escape'", function() {
+
+        // Values for Season 11
+        const seasonData = {
+             maps: ["Storm Point", "World's Edge"],
+             mapDurations: [90, 60, 60, 120, 90, 120],
+             startTime: "2021-11-02T12:00:00Z",
+        };
+
+        // Exactly on rotation
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T12:00:00Z')))
+        .to.equal("World's Edge");
+
+        // Half an hour into a map rotation
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T12:30:00Z')))
+            .to.equal("World's Edge");
+
+        // Dates from a day manually tracking rotations
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T13:00:00Z')))
+            .to.equal("Storm Point");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T15:00:00Z')))
+            .to.equal("World's Edge");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T17:00:00Z')))
+            .to.equal("Storm Point");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T18:30:00Z')))
+            .to.equal("World's Edge");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T20:00:00Z')))
+            .to.equal("Storm Point");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-11T22:00:00Z')))
+            .to.equal("World's Edge");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-12T00:00:00Z')))
+            .to.equal("Storm Point");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-02-11T01:30:00Z')))
+            .to.equal("World's Edge");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-01-12T03:00:00Z')))
+            .to.equal("Storm Point");
+
+        expect(new ApexPlaylist(seasonData).getMapByDate(new Date('2022-02-11T04:00:00Z')))
+            .to.equal("World's Edge");
+    });
+
+});
