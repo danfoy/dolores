789b31ca3d114592d962eda8a791b325293745bb
danfoy <contact@danfoy.com>
2022-11-30 20:35:39 +0000

    build(apexObj): move apexObj to a local depedency

--- commands/map/map.command.js
+++ commands/map/map.command.js
@@ -1,5 +1,4 @@
-const ApexSeason = require('./ApexSeason');
-const apexData = require('./apex.json');
+const apex = require('apexobj');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
@@ -8,17 +7,17 @@ module.exports = {
         description: 'Get the current map schedule for Apex Legends'
     },
 	async execute(interaction) {
-		const seasonData = apexData.seasons[apexData.seasons.length - 1];
-		const battleRoyale = new ApexSeason(seasonData);
+		const battleRoyale = apex.currentSeason.unranked.battleRoyale;
+		console.log(battleRoyale.currentMap);
 
 		const current = battleRoyale.currentMap;
 		const next = battleRoyale.nextMap;
 
 		const response = new MessageEmbed({
 			title: `Current Map: ${current.map}`,
-			description: `*${current.timeRemaining} minutes remaining*`,
+			description: `*${current.timeRemaining / 60} minutes remaining*`,
 			footer: {
-				text: `Next: ${next.map} for ${next.duration} minutes`
+				text: `Next: ${next.map} for ${next.duration / 60} minutes`
 			}
 		});
 
