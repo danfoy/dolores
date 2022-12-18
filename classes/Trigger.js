26c7e77047dcda9e05d52cda119d6b355c428022
danfoy <contact@danfoy.com>
2022-12-18 21:00:31 +0000

    refactor(triggers): reduce closures usage
    Moved a function in the Triggers definition into the class itself rather
    than referencing a function declared outside. This creates a closure,
    and although the performance hit for this is small, this may still
    become an issue when receiving many messages. Also it's cleaner and
    easier to read with the whole function declared as a method on Trigger -
    it's a fairly minimal class anyway.

--- classes/Trigger.js
+++ classes/Trigger.js
@@ -1,15 +1,3 @@
-async function attemptResponse(trigger, message) {
-    try {
-        await trigger._response(message);
-        console.log(
-            `[${trigger.name}] triggered by ${message.author.tag} ` +
-            `in ${message.guild.name}#${message.channel.name}`
-        );
-    } catch (error) {
-        throw new Error(`Trigger ${trigger.name} failed:`, {cause: error});
-    };
-};
-
 module.exports = class Trigger {
     constructor(meta, patterns, response) {
         this.name = meta.name;
@@ -19,7 +7,15 @@ module.exports = class Trigger {
     };
 
     async respond(message) {
-        await attemptResponse(this, message);
+        try {
+            await this._response(message);
+            console.log(
+                `[${this.name}] triggered by ${message.author.tag} ` +
+                `in ${message.guild.name}#${message.channel.name}`
+            );
+        } catch (error) {
+            throw new Error(`Trigger ${this.name} failed:`, {cause: error});
+        };
     };
 
     isMatch(messageContent) {
--- handlers/triggerHandler.js
+++ handlers/triggerHandler.js
@@ -1,5 +1,5 @@
-module.exports = function handleTriggers(message) {
-    message.client.triggers.forEach(trigger => {
+module.exports = async function(message) {
+    await message.client.triggers.forEach(trigger => {
         if (trigger.isMatch(message.content)) trigger.respond(message);
     });
 };
