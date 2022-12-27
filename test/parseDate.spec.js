dad557aa769693dc869002cfb522c8f72508a44a
danfoy <contact@danfoy.com>
2022-12-27 01:52:19 +0000

    refactor(utils)!: rewrite parseDate functions
    Rolled the `isParseableDate` functionality into parseDate, which now
    returns null on failed parses rather than throwing. Throwing errors is
    annoying behaviour; returning null on 'failure' allows for the same
    functionality using guard clauses.
    
    I have also rolled up isDate into the main `parseDate.js` file, renamed
    it to isValidDate, and provided it as a named export. I have kept
    parseDate as the default export because the same functionality is
    largely achievable by checking the result of the latter for truthiness.
    
    BREAKING CHANGE: removed isParseableDate, parseDate now returns null
    when target isn't parseable, isDate renamed to isValidDate and a named
    export on parseDate.

--- test/parseDate.spec.js
+++ test/parseDate.spec.js
@@ -1,15 +1,15 @@
 import { expect } from 'chai';
-import parseDate from '../utils/parseDate.js';
+import  parseDate, { isValidDate } from '../utils/parseDate.js';
 
-describe('util.parseDate(target)', function() {
+describe('parseDate utility', function() {
 
     it('throws if target is not supplied', function() {
         expect(()=>parseDate()).to.throw;
     });
 
-    it('throws if the target is an invalid date ISO string', function() {
-        expect(()=>parseDate('foo')).to.throw;
-        expect(()=>parseDate('2022-13-11T12:00:00Z')).to.throw;
+    it('returns null if the target is not parseable', function() {
+        expect(parseDate('foo')).to.be.null;
+        expect(parseDate('2022-13-11T12:00:00Z')).to.be.null;
     });
 
     it('returns the target if it is already an instance of Date', function() {
@@ -20,5 +20,18 @@ describe('util.parseDate(target)', function() {
 
     it('returns an instance of Date if target is a valid date ISO string', function() {
         expect(parseDate('2022-01-11T12:00:00Z').getMonth()).to.equal(0);
+    });
+});
+
+describe('isValidDate utility', function() {
+
+    it('returns true when target is an instance of Date', function() {
+        expect(isValidDate(new Date())).to.be.true;
+    });
+
+    it('returns false when the target is not an instance of Date', function() {
+        expect(isValidDate('foo')).to.be.false;
+        expect(isValidDate(12)).to.be.false;
+        expect(isValidDate('2022-01-11T12:00:00Z')).to.be.false;
     });
 });
--- utils/parseDate.js
+++ utils/parseDate.js
@@ -1,17 +1,32 @@
-import isDate from './isDate.js';
+/**
+ * Determines whether the input is both an instance of Date and valid - i.e. not
+ * 'Invalid Date' (because an invalid date is still an instance of Date).
+ *
+ * @param {*} target
+ * @returns {boolean}
+ */
+export function isValidDate(target) {
+    return target && // Check date is not falsy
+        Object.prototype.toString.call(target) === '[object Date]' && // Check target is date object
+        target != 'Invalid Date' // Check target is not 'inavlid date';
+};
 
 /**
  * Get a date object or an error. Returns the input if it's already an instance
  * of Date. Attempts to create a new Date object from the supplied argument and
  * returns a new Date instance on success or an Error on failure.
  *
+ * @export
  * @param {Date|ISO date string} target
- * @returns {Date|Error}
+ * @returns {Date|null}
  */
 export default function parseDate(target) {
-    if (!target) throw new Error(`Target date is required and not provided`);
-    if(isDate(target)) return target;
-    const newDate = new Date(target);
-    if (!isDate(newDate)) throw new Error(`Unable to parse date from ${target}`);
-    return newDate;
+    if (!target) throw new Error(`Missing argument`);
+
+    if (isValidDate(target)) return target;
+
+    const parsedDate = new Date(target);
+    if (isValidDate(parsedDate)) return parsedDate;
+
+    return null;
 };
